b811865606daf90e9095a13ef21e8f72d6b71f6e
Matheus Braga <sbmatheus20@gmail.com>
2026-01-18 12:21:57 -0300

    Executando Typescript com tsx

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ export function sum(a: number, b: number): number {
     return a + b; 
 }
 
-const result: number = sum(5, 3)
+const result: number = sum(5, 7)
 console.log("Resultado da soma", result)
